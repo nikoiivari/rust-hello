b57be32423a8400107e5f332fa10df6398158d9b
nikoiivari <nxu@huoleton.net>
2021-09-29 12:33:19 +0300

    Init SDL2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
-// This is a comment, and is ignored by the compiler
+extern crate sdl2;
 
-// This is the main function
+//use sdl2::event::Event;
+//use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+//use std::time::Duration;
+
 fn main() {
-    // Statements here are executed when the compiled binary is called
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("Hello!", 800, 600)
+        .build()
+        .unwrap();
+    
+    let mut canvas = window.into_canvas()
+        .build()
+        .unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
 
     // Print text to the console
     println!("Hello World!");
