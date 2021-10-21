63059cac033daf172cc586816b230f74eea58aee
Niko Iivari <nxu@huoleton.net>
2021-10-21 11:36:46 +0300

    add vulkan-test.rs

---
+++ src/bin/vulkan-test.rs
@@ -0,0 +1,15 @@
+
+use std::time::Duration;
+
+fn main() {
+
+    // Initialize Vulkan
+
+    // Mainloop
+    'running: loop {
+        
+        
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 480));
+    }
+    // Cleanup
+}
