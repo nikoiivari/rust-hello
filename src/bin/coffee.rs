49b8e8d0292cc2f6150bf9aecab4d9e358174131
Niko Iivari <nxu@huoleton.net>
2021-12-02 11:49:56 +0200

    coffee improvements

--- src/bin/coffee.rs
+++ src/bin/coffee.rs
@@ -18,20 +18,29 @@ fn main() {
 
         let userstring = get_user_string();
         let userinput = parse_user_input(userstring);
-        if let UserInput::Exit = userinput {println!("Poistut kahvilasta.");break;}
+        println!(""); // Makes output prettier
+        if let UserInput::Exit = userinput {
+            println!("Poistut kahvilasta asianmukaisesti.");
+            break;}
         else if let UserInput::Virhe = userinput {
             println!("Valitsit väärin. Sinut heitetään ulos kahvilasta.");
             break;
         }
         else if let UserInput::Kylla = userinput {
-            userwallet = userwallet - coffee_price;
-            println!("Ostit kahvin. Lompakkoon jäi rahaa: {}€", userwallet);
-            println!("\n");
+            if userwallet >= coffee_price {
+                userwallet = userwallet - coffee_price;
+                println!("Ostit kahvin. Lompakkoon jäi rahaa: {}€", userwallet);
+                println!("");                
+            } else {
+                println!("Lompakossa ei ole tarpeeksi rahaa kahvin ostamiseen.");
+                println!("Ostotapahtuma keskeytyy.");
+                println!("");
+            }
         }
         else if let UserInput::Ei = userinput {
             println!("Et ostanut kahvia. Myyjä katselee sinua harmistuneena. Kulutat kahvilan");
             println!("hengitysilmaa, ja viet yhden istumapaikan tuottamatta tuloja kahvilalle.");
-            println!("\n");
+            println!("");
         }
     }
 
