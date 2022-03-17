f3ce2036d3b6cbfb35fc79bcf02287d02ccc0200
nikoiivari <nxu@huoleton.net>
2022-03-17 12:42:17 +0200

    Make window resizable

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -1,6 +1,7 @@
 extern crate sdl2;
 
 use sdl2::event::Event;
+use sdl2::event::WindowEvent;
 use sdl2::keyboard::Keycode;
 use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
@@ -10,9 +11,11 @@ fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("Hello!", 800, 600)
-        .build()
-        .unwrap();
+    let mut winbuild = sdl2::video::WindowBuilder::new(&video_subsystem,"Test", 800, 600);
+    winbuild.resizable();
+    let window = winbuild.build().unwrap();
+
+    //let window = video_subsystem.window("Hello!", 800, 600).build().unwrap();
 
     let mut canvas = window.into_canvas()
         .build()
@@ -27,6 +30,8 @@ fn main() {
     let mut mousex = 0;
     let mut mousey = 0;
     let mut mouseb = 0;
+    let mut winw = 800;
+    let mut winh = 600;
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -34,23 +39,31 @@ fn main() {
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running
                 },
-                Event::Window {timestamp: _, window_id: _, win_event: _} => {
-                    //TODO: Only print sth when window is resized
-                    println!("Hello World");
+                Event::Window {timestamp: _, window_id: _, win_event: wev} => {
+                    match wev {
+                        WindowEvent::SizeChanged(winw, winh) => {
+
+                             println!("Size changed {}, {}", winw, winh);
+                        }
+                        _ => {}    
+                    }
+                    
                 },
                 Event::KeyUp { keycode: Some(Keycode::Space), .. } => {
                     println!("Space");
                 },
                 //Event::MouseButtonDown {} => 
                 Event::MouseButtonUp {mouse_btn:mouseb, x:mousex, y:mousey, .. } => {
-                    if (MouseButton::Left == mouseb) {
+                    if MouseButton::Left == mouseb {
                         println!("Mouse x:{}, y:{}", mousex, mousey);
                     }
                 }
                 _ => {}
             }
         }
-
+        
+        //canvas.set_draw_color(Color::RGB(0, 127, 127));
+        canvas.clear();
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 480));
     }
