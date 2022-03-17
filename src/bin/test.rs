b88917787a1698223911f787fd388b315517dd46
nikoiivari <nxu@huoleton.net>
2022-03-17 11:19:55 +0200

    Detect left mouse button

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -2,6 +2,7 @@ extern crate sdl2;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::mouse::MouseButton;
 use sdl2::pixels::Color;
 use std::time::Duration;
 
@@ -25,6 +26,7 @@ fn main() {
 
     let mut mousex = 0;
     let mut mousey = 0;
+    let mut mouseb = 0;
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -40,8 +42,10 @@ fn main() {
                     println!("Space");
                 },
                 //Event::MouseButtonDown {} => 
-                Event::MouseButtonUp { x:mousex, y:mousey, .. } => {
-                    println!("Mouse x:{}, y:{}", mousex, mousey);
+                Event::MouseButtonUp {mouse_btn:mouseb, x:mousex, y:mousey, .. } => {
+                    if (MouseButton::Left == mouseb) {
+                        println!("Mouse x:{}, y:{}", mousex, mousey);
+                    }
                 }
                 _ => {}
             }
