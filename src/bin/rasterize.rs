043b255fb9dffa62fb31e4a12fcde9b396304cb3
nikoiivari <nxu@huoleton.net>
2022-04-07 12:34:09 +0300

    rasterize.rs builds with warnings

--- src/bin/rasterize.rs
+++ src/bin/rasterize.rs
@@ -1,6 +1,8 @@
+extern crate obj;
+
 use std::fs::File;
 use std::io::BufReader;
-use obj::{load_obj, Obj};
+use obj::{load_obj, Obj, ObjError};
 
 struct RGBA {
     r: u8,
@@ -13,23 +15,23 @@ struct Square {
     minx: f32,
     maxx: f32,
     miny: f32,
-    minx: f32,
+    maxy: f32,
 }
 
-let input = BufReader::new(File::open("objfile.obj")?);
-let mesh: Obj = load_obj(input)?;
 
-
-fn main () {
-    
+fn main () -> Result<(), ObjError> {
+    let input = BufReader::new(File::open("objfile.obj")?);
+    let mesh: Obj = load_obj(input)?;
+    Ok(())
 }
 
-fn verts_in_square (sq: Square) -> u16 {
+fn verts_in_square (sq: Square, mesh: Obj) -> u16 {
     let count: u16 = 0;
     
     for vert in mesh.vertices{
 
     }
+    return count;
 }
 
 fn default_rgba () -> RGBA {
@@ -38,5 +40,5 @@ fn default_rgba () -> RGBA {
         g: 0,
         b: 0,
         a: 0,
-    };
+    }
 }
