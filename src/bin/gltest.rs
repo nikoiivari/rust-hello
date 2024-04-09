df0eac286fbaadb149f1f4adeaffdc8e2fef2be7
Niko Iivari <nxu@huoleton.net>
2024-04-09 12:10:19 +0300

    print shader compilation error

--- src/bin/gltest.rs
+++ src/bin/gltest.rs
@@ -7,7 +7,7 @@ use sdl2::keyboard::Keycode;
 use sdl2::mouse::MouseButton;
 //use sdl2::video::GLProfile;
 
-use gl::types::GLuint;
+use gl::types::{GLuint, GLint, GLchar};
 
 use std::time::Duration;
 use std::ffi::{CString, CStr};
@@ -29,7 +29,8 @@ fn main() {
 
     let mut event_pump = sdl_context.event_pump().unwrap();
     let vs = build_vertex_shader();
-    //let fs = build_fragment_shader();
+    let fs = build_fragment_shader();
+    let prog = link_shaders(vs, fs);
 
     'running: loop {
         for event in event_pump.poll_iter() {
@@ -79,15 +80,15 @@ fn main() {
             gl::BindBuffer(gl::ARRAY_BUFFER, vertexbuffer);
             //println!("VertexBufferId: {vertexbuffer}");
             
-            //gl::EnableVertexAttribArray(0); //index
-            //gl::VertexAttribPointer(
-            //                        0, //index
-            //                        3,
-            //                        gl::FLOAT,
-            //                        gl::FALSE,
-            //                        (3 * std::mem::size_of::<f32>()) as gl::types::GLint,
-            //                        ()
-            //                        );
+            gl::EnableVertexAttribArray(0); //index
+            gl::VertexAttribPointer(
+                                    0, //index
+                                    3,
+                                    gl::FLOAT,
+                                    gl::FALSE,
+                                    0,
+                                    std::ptr::null()
+            );
             
             gl::BufferData(
                 gl::ARRAY_BUFFER,
@@ -96,8 +97,9 @@ fn main() {
                 gl::STATIC_DRAW,
             );
 
+            gl::UseProgram(prog);
             gl::DrawArrays(gl::TRIANGLES, 0, 3);
-            //gl::DisableVertexAttribArray(0);
+            gl::DisableVertexAttribArray(0);
 
             //release stuff
             gl::DeleteVertexArrays(1, &mut vertexarrayid);
@@ -113,8 +115,8 @@ fn main() {
 fn build_vertex_shader() -> GLuint {
     let vshader:GLuint = unsafe { gl::CreateShader(gl::VERTEX_SHADER) };
 
-    let source: &CStr  = CStr::from_bytes_with_nul(b"#version 330
-    layout(location = 0) in vec3 vertexPos
+    let source: &CStr  = CStr::from_bytes_with_nul(b"#version 330 core
+    layout(location = 0) in vec3 vertexPos;
     void main (){
         gl_Position.xyz = vertexPos;
         gl_Position.w = 1.0;
@@ -123,14 +125,31 @@ fn build_vertex_shader() -> GLuint {
     unsafe {
         gl::ShaderSource(vshader, 1, &source.as_ptr(), std::ptr::null());
         gl::CompileShader(vshader);
+        let mut success: GLint = 1;
+        gl::GetShaderiv(vshader, gl::COMPILE_STATUS, &mut success);
+        if success == 0 {
+            let mut len:GLint = 0;
+            gl::GetShaderiv(vshader, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf: Vec<u8> = Vec::with_capacity(len as usize +1);
+            buf.extend([b' '].iter().cycle().take(len as usize));
+            let error: CString = CString::from_vec_unchecked(buf);
+            gl::GetShaderInfoLog(
+                vshader,
+                len,
+                std::ptr::null_mut(),
+                error.as_ptr() as *mut GLchar
+            );
+            println!("{}", error.to_string_lossy().into_owned());
+        }
     }
+
     return vshader;
 }
 
 fn build_fragment_shader() -> GLuint {
     let fshader:GLuint = unsafe { gl::CreateShader(gl::FRAGMENT_SHADER) };
 
-    let source: &CStr  = CStr::from_bytes_with_nul(b"#version 330
+    let source: &CStr  = CStr::from_bytes_with_nul(b"#version 330 core
     out vec4 fragColor;
     void main (){
         fragColor = vec4(1.0, 0.0, 0.0, 1.0);
@@ -139,6 +158,33 @@ fn build_fragment_shader() -> GLuint {
     unsafe {
         gl::ShaderSource(fshader, 1, &source.as_ptr(), std::ptr::null());
         gl::CompileShader(fshader);
+        let mut success: GLint = 1;
+        gl::GetShaderiv(fshader, gl::COMPILE_STATUS, &mut success);
+        if success == 0 {
+            let mut len:GLint = 0;
+            gl::GetShaderiv(fshader, gl::INFO_LOG_LENGTH, &mut len);
+            let mut buf: Vec<u8> = Vec::with_capacity(len as usize +1);
+            buf.extend([b' '].iter().cycle().take(len as usize));
+            let error: CString = CString::from_vec_unchecked(buf);
+            gl::GetShaderInfoLog(
+                fshader,
+                len,
+                std::ptr::null_mut(),
+                error.as_ptr() as *mut GLchar
+            );
+            println!("{}", error.to_string_lossy().into_owned());
+        }
     }
     return fshader;
+}
+
+fn link_shaders(vs:GLuint, fs:GLuint) -> GLuint {
+    let prog:GLuint = unsafe { gl::CreateProgram() };
+
+    unsafe {
+        gl::AttachShader(prog, vs);
+        gl::AttachShader(prog, fs);
+        gl::LinkProgram(prog);
+    }
+    return prog;
 }
