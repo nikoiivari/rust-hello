51fc435e540cdaae1e5741c58a04fc65440e455d
Niko Iivari <nxu@huoleton.net>
2024-07-23 13:51:15 +0300

    byteasm: open input file.

--- src/bin/byteasm.rs
+++ src/bin/byteasm.rs
@@ -1,7 +1,15 @@
-use std::path::Path;
+use std::env;
+//use std::path::Path;
 use std::fs::File;
 
 fn main ()
 {
     println!("Hello, bytecode assembler.");
+
+    //commandline args
+    let args: Vec<String> = env::args().collect();
+    //println!("{:?}", args);
+    let infilepath = &args[1];
+    let infile = File::open(infilepath).unwrap();
+
 }
