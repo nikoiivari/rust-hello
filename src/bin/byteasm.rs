f76b626ff49094501fc336e24b5e200b2a92ec40
Niko Iivari <nxu@huoleton.net>
2024-08-19 13:59:56 +0300

    nand()

--- src/bin/byteasm.rs
+++ src/bin/byteasm.rs
@@ -2,14 +2,28 @@ use std::env;
 //use std::path::Path;
 use std::fs::File;
 
+fn nand(ina:u64, inb:u64) -> u64
+{
+    return !(ina & inb);
+}
+
+fn test_nand_adder()
+{
+
+}
+
 fn main ()
 {
-    println!("Hello, bytecode assembler.");
+    let rval:u64 = nand(0b1111, 0b1111);
+    println!("Hello, bytecode assembler. rval = {rval:#b}");
 
     //commandline args
     let args: Vec<String> = env::args().collect();
-    //println!("{:?}", args);
-    let infilepath = &args[1];
-    let infile = File::open(infilepath).unwrap();
+    if 1 < args.len()
+    {
+        //println!("{:?}", args);
+        let infilepath = &args[1];
+        //let infile = File::open(infilepath).unwrap();
+    }
 
 }
