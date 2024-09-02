f320bee2844b802234a3ed12380cb23ca935ac5d
Niko Iivari <nxu@huoleton.net>
2024-09-02 14:58:07 +0300

    move test_2bit_nand_adder() to foo.txt

--- src/bin/byteasm.rs
+++ src/bin/byteasm.rs
@@ -1,98 +1,12 @@
 use std::env;
 //use std::path::Path;
-//use std::fs::File;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn nand(ina:u64, inb:u64) -> u64
 {
     return !(ina & inb);
 }
-
-fn test_2bit_nand_adder(ina:u64, inb:u64) -> (u64, u64)
-{
-    // Split bits to add them separately using one bit adder.
-    let inabit0:u64 = ina & 0b1;
-    let inabit1bs:u64 = ina & 0b10;
-    let inabit1:u64 = inabit1bs >>1;
-    println!("inabit0: {inabit0:#b}");
-    println!("inabit1: {inabit1:#b}");
-    
-    let inbbit0:u64 = inb & 0b1;
-    let inbbit1bs:u64 = inb & 0b10;
-    let inbbit1:u64 = inbbit1bs >>1;
-    println!("inbbit0: {inbbit0:#b}");
-    println!("inbbit1: {inbbit1:#b}");
-
-    let mut cin:u64 = 0;
-    let mut sum:u64; 
-    let cout:u64;
-
-        {
-        // 1-bit adder begins
-        let tmp1 = nand(inabit0, inbbit0);
-        println!("tmp1: {tmp1:#b}");
-
-        let tmp2 = nand(inabit0, tmp1);
-        println!("tmp2: {tmp2:#b}");
-        let tmp3 = nand(inbbit0, tmp1);
-        println!("tmp3: {tmp3:#b}");
-
-        let tmp4 = nand(tmp2, tmp3);
-        println!("tmp4: {tmp4:#b}");
-        
-        println!("cin:  {cin:#b}");
-        let tmp5 = nand(tmp4, cin);
-        println!("tmp5: {tmp5:#b}");
-        
-        let tmp6 = nand(tmp4, tmp5);
-        println!("tmp6: {tmp6:#b}");
-        let tmp7 = nand(tmp5, cin);
-        println!("tmp7: {tmp7:#b}");
-
-        let tmp8sum = nand(tmp6, tmp7);
-        println!("tmp8sum: {tmp8sum:#b}");
-        let tmp9cout = nand(tmp5, tmp1);
-        println!("tmp9cout: {tmp9cout:#b}");
-        // 1-bit adder ends
-
-        sum = tmp8sum;
-        cin = tmp9cout;
-        }
-
-        {
-        // 1-bit adder begins
-        let tmp1 = nand(inabit1, inbbit1);
-        println!("tmp1: {tmp1:#b}");
-
-        let tmp2 = nand(inabit1, tmp1);
-        println!("tmp2: {tmp2:#b}");
-        let tmp3 = nand(inbbit1, tmp1);
-        println!("tmp3: {tmp3:#b}");
-
-        let tmp4 = nand(tmp2, tmp3);
-        println!("tmp4: {tmp4:#b}");
-        
-        println!("cin:  {cin:#b}");
-        let tmp5 = nand(tmp4, cin);
-        println!("tmp5: {tmp5:#b}");
-        
-        let tmp6 = nand(tmp4, tmp5);
-        println!("tmp6: {tmp6:#b}");
-        let tmp7 = nand(tmp5, cin);
-        println!("tmp7: {tmp7:#b}");
-
-        let tmp8sum = nand(tmp6, tmp7);
-        println!("tmp8sum: {tmp8sum:#b}");
-        let tmp9cout = nand(tmp5, tmp1);
-        println!("tmp9cout: {tmp9cout:#b}");
-        // 1-bit adder ends
-
-        //combine bits
-        sum = sum | (tmp8sum<<1); //OR-gate can be implemented with NAND-gates
-        cout = tmp9cout;
-        }
-    return (sum, cout);
-}
-
 
 
 fn main ()
@@ -111,8 +25,13 @@ fn main ()
     if 1 < args.len()
     {
         println!("{:?}", args);
-        //let infilepath = &args[1];
-        //let infile = File::open(infilepath).unwrap();
-    }
+        let infilepath = &args[1];
+        let infile = File::open(infilepath).unwrap();
 
+        let reader = BufReader::new(infile);
+
+        for line in reader.lines() {
+            println!("{:?}", line);
+        }
+    }
 }
