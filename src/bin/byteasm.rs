1a9fff9a850b6b2ed7be3ffcaccf8660f28ece5c
Niko Iivari <nxu@huoleton.net>
2024-09-02 15:29:56 +0300

    allow dead code

--- src/bin/byteasm.rs
+++ src/bin/byteasm.rs
@@ -1,37 +1,35 @@
+#![allow(dead_code, unused_imports)]
+
 use std::env;
 //use std::path::Path;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::Read;
 
 fn nand(ina:u64, inb:u64) -> u64
 {
     return !(ina & inb);
 }
 
-
 fn main ()
 {
     //let rval:u64 = nand(0b1111, 0b1111);
     //println!("nand(0b1111, 0b1111) = {rval:#b}");
-    let rtuple:(u64, u64) = test_2bit_nand_adder(1, 2);
-    //let rtuple:(u64, u64) = ;
-    let rsum:u64;
-    let rcarry:u64;
-    (rsum, rcarry) = rtuple;
-    println!("Sum: {rsum}\nCarry: {rcarry}");
-
+    
     //commandline args
     let args: Vec<String> = env::args().collect();
     if 1 < args.len()
     {
-        println!("{:?}", args);
+        //println!("{:?}", args);
         let infilepath = &args[1];
-        let infile = File::open(infilepath).unwrap();
+        let mut infile = File::open(infilepath).unwrap();
 
-        let reader = BufReader::new(infile);
+        //let reader = BufReader::new(infile);
+        let mut s: String = Default::default();
+        let _ = infile.read_to_string(&mut s);
 
-        for line in reader.lines() {
-            println!("{:?}", line);
-        }
+        //for line in reader.lines() {
+        //    println!("{:?}", line);
+        //}
+        println!("{}", s);
     }
 }
