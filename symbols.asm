f4a04baa2c4e4f13bb8aefba8f8021b7912607bf
Niko Iivari <nxu@huoleton.net>
2024-09-10 15:55:17 +0300

    Add section on indexing.

--- symbols.asm
+++ symbols.asm
@@ -4,5 +4,10 @@
 
 # Start with variable definitions. These are similiar to labels,
 # and have a colon ':' after the variable name.
-foo     : .stuv, 4byte, unsigned
-bar     : .wxyz, 4byte, unsigned # .wxyz gets packed into the previous dword.
+accum   : .stuvwxyz,    8byte, capability
+cache   : .stuvwxyz,    8byte, capability
+foo     : .stuv,        4byte, unsigned
+bar     : .wxyz,        4byte, unsigned # .wxyz gets packed into the previous dword.
+
+accum = @
+@ = @[:0...3]       # Square brackets '[' and ']' imply indexing -- not direct memory access
